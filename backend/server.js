adddfaaeb56e737a4e281715699ca07b1aaa00bc
tenzinkunkyab <tenzinkunkyab5@gmail.com>
2024-04-09 20:19:56 -0400

    created controller folder

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+require('dotenv').config()
+const mongoose = require("mongoose")
+
+const workoutRoutes = require('./routes/workout');
+
+
+const app = express();
+
+
+
+app.use(express.json())
+
+
+app.use((req,res,next)=>{
+
+   console.log(req.path, req.method);
+   next()
+})
+
+
+ 
+//ROUTE
+app.use("/api/workouts",workoutRoutes)
+
+//connect to db
+
+mongoose.connect(process.env.MONGO_URI)
+
+.then(()=>{
+  app.listen(process.env.PORT,()=>{
+    console.log("connected to db and server running on",process.env.PORT);
+  })
+})
+.catch((error)=>{
+console.error(error);
+})
+
